fdf53ee14457179c28d719a9d01603fbd7904d2a
Abhinav P S <146312343+abiinavv@users.noreply.github.com>
2024-12-16 22:52:27 +0530

    Update Drawer.js

--- src/Pages/Developers/Drawer.js
+++ src/Pages/Developers/Drawer.js
@@ -8,22 +8,20 @@ import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { getSkills, getTags } from "../../Firebase/firebase";
-import "./Developers.scss";
 import { TextField, Typography } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import { ThemeContext } from "../../App";
 import { ProjectContext } from "../../contexts/ProjectContext";
 import { useContext } from "react";
+
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
-
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
   justifyContent: "flex-end",
 }));
+
 const styles = {
   fontFamily: "Nunito",
   fontWeight: "450",
@@ -33,6 +31,7 @@ const styles = {
   padding: "0",
   margin: "0",
 };
+
 const styles1 = {
   fontFamily: "Nunito",
   fontWeight: "600",
@@ -42,6 +41,7 @@ const styles1 = {
   padding: "0",
   margin: "0",
 };
+
 export default function PersistentDrawerLeft({
   selectedSkills,
   setSelectedSkills,
@@ -53,12 +53,8 @@ export default function PersistentDrawerLeft({
   open,
   setOpen,
 }) {
-  const { branch, setBranch, yop, setYop, setWidth } = React.useContext(
-    ThemeContext
-  );
-  const {tags, skills} = useContext(ProjectContext)
-  // const [branch, setBranch] =  React.useState('')
-  // const [yop, setYop] = React.useState('')
+  const { branch, setBranch, yop, setYop, setWidth } = React.useContext(ThemeContext);
+  const { tags, skills } = useContext(ProjectContext);
 
   const [skillList, setSkillList] = React.useState([
     "React",
@@ -67,21 +63,15 @@ export default function PersistentDrawerLeft({
     "C++",
   ]);
 
-
-  // const [renderedFilters, setRenderedFilters] = useState([]);
-  // useEffect(() => {
-  //   setRenderedFilters([...selectedSkills, ...selectedTags]);
-  // }, [selectedSkills, selectedTags]);
-
   const branches = ["CSE", "ECE", "EEE", "EBE", "MECH"];
-  // const [branch, setBranch] = React.useState(branches);
   const years = ["2023", "2024", "2025", "2026"];
+
   const matches0 = useMediaQuery("(max-width:500px)");
   const matches1 = useMediaQuery("(max-width:600px)");
   const matches2 = useMediaQuery("(max-width:800px)");
   const matches3 = useMediaQuery("(max-width:865px)");
-  const matches4 = useMediaQuery("(max-width:380px)");
   const matches5 = useMediaQuery("(max-width:1000px)");
+
   const drawerWidth = matches5
     ? matches2
       ? matches1
@@ -99,12 +89,10 @@ export default function PersistentDrawerLeft({
     setOpen(true);
   };
 
-  const handleDrawerClose = (event, reason) => {
-    if (reason === "backdropClick") {
-      setOpen(false);
-    }
+  const handleDrawerClose = () => {
     setOpen(false);
   };
+
   const addSkill = (skill) => {
     let oldSkills = selectedSkills;
     if (oldSkills.find((s) => s === skill)) {
@@ -114,6 +102,7 @@ export default function PersistentDrawerLeft({
     }
     setSelectedSkills(oldSkills);
   };
+
   function handleChange(event) {
     const Name = event.target.value;
     setSearch(Name);
@@ -127,6 +116,7 @@ export default function PersistentDrawerLeft({
       );
     }
   }
+
   const addTag = (tag) => {
     let oldTags = selectedTags;
     if (oldTags.find((s) => s === tag)) {
@@ -138,55 +128,45 @@ export default function PersistentDrawerLeft({
   };
 
   const clearFilter = () => {
-    setCleared(!cleared);
-    // // console.log(selectedSkills)
-    const page = window.location.href.split("/")[3];
-    // // console.log(page)
-    if (page === "projects") {
-      setSelectedSkills([]);
-      setSelectedTags([]);
-    }
-    if (page === "developers") {
-      setBranch([]);
-      setYop([]);
-      setSelectedSkills([]);
-      // setSelectedTags([]);
-    }
+    setSelectedSkills([]);
+    setSelectedTags([]);
+    setBranch([]);
+    setYop([]);
   };
-  const [cleared, setCleared] = React.useState(false);
 
   return (
     <>
+      {/* Static Filter Button */}
       <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          // mr: 5,
-          color: "#9e0000",
-          position: "fixed",
-          left: matches4 ? "15px" : "30px",
-          top: matches3 ? "184px" : "140px",
-          display: open && "none",
-          borderRadius: "5px",
-          "&:hover": {
-            backgroundColor: "transparent",
-          },
-        }}
-        className="menu_button"
-      >
-        <FilterAltIcon sx={{ fontSize: "2rem" }} />
-        <Typography
-          sx={{ padding: "0px", fontSize: "1.5rem", fontWeight: "500" }}
-        >
-          Filter
-        </Typography>
-      </IconButton>
+  color="inherit"
+  aria-label="open drawer"
+  onClick={handleDrawerOpen}
+  edge="start"
+  sx={{
+    position: "relative", 
+    marginTop: "20px", 
+    marginLeft: "20px", 
+    backgroundColor: "transparent", 
+    color: "#9e0000", 
+    padding: "10px",
+    borderRadius: "50%", 
+    "&:hover": {
+      backgroundColor: "rgba(0, 0, 0, 0.1)", 
+    },
+  }}
+>
+  <FilterAltIcon sx={{ fontSize: "2rem" }} />
+  <Typography sx={{ paddingLeft: "8px", fontSize: "1.5rem", fontWeight: "500", color: "#9e0000" }}>
+    Filter
+  </Typography>
+</IconButton>
 
+
+
+
+
+
+      {/* Drawer */}
       <Drawer
         sx={{
           width: drawerWidth,
@@ -197,21 +177,15 @@ export default function PersistentDrawerLeft({
             background: "linear-gradient(90deg, #8B1010 0%, #C71111 100%)",
             borderBottomRightRadius: "16px",
             borderTopRightRadius: "16px",
-            bottom: "0",
             height: "100%",
           },
         }}
-        // variant="persistent"
         anchor="left"
         open={open}
-        onClose={(event, reason) => {
-          if (reason === "backdropClick") {
-            setOpen(false);
-          }
-        }}
+        onClose={handleDrawerClose}
       >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerClose} sx={{ position: "fixed" }}>
+          <IconButton onClick={handleDrawerClose}>
             {theme.direction === "ltr" ? (
               <ChevronLeftIcon sx={{ fontSize: "2rem", color: "white" }} />
             ) : (
@@ -221,291 +195,22 @@ export default function PersistentDrawerLeft({
         </DrawerHeader>
         <Divider />
         <div className="filter_box">
-          <div
+          <h3 style={styles1}>Filter By:</h3>
+          <button
             style={{
-              minWidth: "90%",
-              margin: "15px",
-              display: page === "Projects" ? "flex" : "none",
+              color: "white",
+              padding: "1px 7px",
+              fontSize: "16px",
+              borderRadius: "5px",
+              backgroundColor: "transparent",
+              border: "2px solid white",
+              fontStyle: "Nunito",
             }}
+            onClick={clearFilter}
           >
-            <h3 style={styles}>Search By:</h3>
-          </div>
-          <div
-            style={{
-              minWidth: "90%",
-              margin: "25px",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <h3 style={styles1}>Filter By:</h3>
-            <button
-              style={{
-                color: "white",
-                padding: "1px 7px",
-                fontSize: "16px",
-                borderRadius: "5px",
-                backgroundColor: "transparent",
-                border: "2px solid white",
-                fontStyle: "Nunito",
-                // hover: "#9e0000",
-              }}
-              onClick={() => {
-                clearFilter();
-              }}
-              // onMouseEnter={(e) => (e.target.style.backgroundColor = "#FF0000")}
-              // onMouseLeave={(e) => (e.target.style.backgroundColor = "#9e0000")}
-            >
-              Clear Filter
-            </button>
-          </div>
-
-          <div
-            style={{
-              minWidth: "90%",
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "centre",
-              flexDirection: "row",
-            }}
-          >
-            <h3 style={styles}>{page === "Projects" ? "Tech Stacks" : null}</h3>
-          </div>
-          {/* <div style={{ minWidth: "90%", margin: "25px" }}>
-            <h3 style={styles}>Branch/Class</h3>
-          </div> */}
-          {/* <input type="text" className="input_box" /> */}
-          <div className="skills">
-            <div className="skills" style={{ width: "100%" }}>
-              <input
-                name="Skills"
-                value={search}
-                onChange={handleChange}
-                style={{
-                  width: "80%",
-                  margin: "20px 0 20px 0",
-                  fontFamily: "Nunito",
-                  background: "transparent",
-                  color: "white",
-                  border: "2px solid #D9D9D9",
-                  borderRadius: "5px",
-                  padding: "15px 20px",
-                  borderColor: "white",
-                }}
-                // InputProps={{ style: { color: "white", fontFamily: "Nunito" } }}
-                // InputLabelProps={{
-                //   style: { color: "white", fontFamily: "Nunito" },
-                // }}
-                placeholder="Start typing ..."
-              />
-            </div>
-            {/* <input
-            type="text"
-            placeholder="Start typing..."
-            className="input_box"
-          /> */}
-            <div style={{ width: "90%" }}>
-              <div className="skills">
-                {skillList.map((x, id) => (
-                  <Buttons
-                    key={id}
-                    name={x}
-                    className="skill_boxes"
-                    addSkills={addSkill}
-                    tags={selectedTags}
-                    clearFilter={cleared}
-                  ></Buttons>
-                ))}
-              </div>
-            </div>
-            {/* {
-              cleared ? setCleared(false) : null
-            }  */}
-            {/* <div
-            style={
-              page === "Projects"
-                ? { display: "none" }
-                : { minWidth: "90%", margin: "25px" }
-            }
-          >
-            <h3 style={styles}>Branch/Class</h3>
-          </div> */}
-            {/* <input type="text" className="input_box" /> */}
-            <div
-              className="skills"
-              style={{
-                display: page === "Projects" ? "none" : "flex",
-              }}
-            >
-              <Autocomplete
-                id="Branch"
-                multiple
-                onChange={(event, value) => setBranch(value)}
-                name="Branch"
-                options={branches}
-                value={branch}
-                sx={{ width: "80%", margin: "1.5rem 0" }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Branch/Class"
-                    required
-                    sx={{
-                      "& .MuiChip-deleteIcon": {
-                        color: "white !important",
-                        background: "transparent",
-                      },
-                      "& .MuiOutlinedInput-root": {
-                        "& .MuiAutocomplete-endAdornment .MuiButtonBase-root .MuiSvgIcon-root": {
-                          color: "white",
-                        },
-                        "& fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                        "& .MuiChip-root": {
-                          color: "white",
-                          background: "transparent",
-                          border: "2px solid #D9D9D9",
-                        },
-                        "&.Mui-focused fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                      },
-                      "& .MuiOutlinedInput-root:hover": {
-                        "& fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                      },
-                      "& label": {
-                        color: "white",
-                      },
-                      "& label.Mui-focused": {
-                        color: "white",
-                      },
-                      "& .MuiOutlinedInput-input ": {
-                        color: "white",
-                      },
-                    }}
-                  />
-                )}
-              />
-            </div>
-            {/* placeholder='Start typing...' */}
-            {/* <div
-            style={
-              page === "Projects"
-                ? { display: "none" }
-                : { minWidth: "90%", margin: "25px" }
-            }
-          >
-            <h3 style={styles}>Year of passing</h3>
-          </div>  */}
-            {/* <input type="text" className="input_box" /> */}
-            <div
-              className="skills"
-              style={{
-                display: page === "Projects" ? "none" : "flex",
-              }}
-            >
-              {/* {years.map((x, id) => (
-              <Buttons
-                key={id}
-                name={x}
-                className="skill_boxes menu_button"
-                addSkills={addYop}
-              ></Buttons>
-            ))} */}
-              <Autocomplete
-                id="Year"
-                multiple
-                onChange={(event, value) => setYop(value)}
-                name="Year"
-                options={years}
-                value={yop}
-                sx={{ width: "80%", margin: "1.5rem 0" }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Year of passing"
-                    required
-                    sx={{
-                      "& .MuiChip-deleteIcon": {
-                        color: "white !important",
-                        background: "transparent",
-                      },
-                      "& .MuiOutlinedInput-root": {
-                        "& .MuiAutocomplete-endAdornment .MuiButtonBase-root .MuiSvgIcon-root": {
-                          color: "white",
-                        },
-                        "& fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                        "& .MuiChip-root": {
-                          color: "white",
-                          background: "transparent",
-                          border: "2px solid #D9D9D9",
-                        },
-                        "&.Mui-focused fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                      },
-                      "& .MuiOutlinedInput-root:hover": {
-                        "& fieldset": {
-                          border: "2px solid #D9D9D9",
-                          color: "white",
-                        },
-                      },
-                      "& label": {
-                        color: "white",
-                      },
-                      "& label.Mui-focused": {
-                        color: "white",
-                      },
-                      "& .MuiOutlinedInput-input ": {
-                        color: "white",
-                      },
-                    }}
-                  />
-                )}
-              />
-            </div>
-            <div
-              style={
-                page === "Projects"
-                  ? { minWidth: "90%", margin: "25px" }
-                  : { display: "none" }
-              }
-            >
-              <h3 style={styles}>Tags</h3>
-            </div>
-            {/* <input type="text" className="input_box" /> */}
-            <div
-              className="skills"
-              style={{
-                display: page === "Projects" ? "flex" : "none",
-              }}
-            >
-              {page === "Projects"
-                ? tags?.map((x, id) => (
-                    <Buttons
-                      page={"Projects"}
-                      key={id}
-                      name={x}
-                      className="skill_boxes menu_button"
-                      addSkills={addTag}
-                      tags={selectedTags}
-                      clearFilter={cleared}
-                    ></Buttons>
-                  ))
-                : null}
-            </div>
-          </div>
+            Clear Filter
+          </button>
+          {/* Filter content */}
         </div>
       </Drawer>
     </>
